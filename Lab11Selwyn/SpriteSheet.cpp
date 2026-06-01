1034d7ab29b61cfa5f91593d09c433e54d64cb50
Tyler <tylerselwyn427@gmail.com>
2026-06-01 16:48:16 -0400

    Adjusting speed

--- Lab11Selwyn/SpriteSheet.cpp
+++ Lab11Selwyn/SpriteSheet.cpp
@@ -46,7 +46,7 @@ void Sprite::UpdateSprites(int width, int height, int dir)
 
 	if (dir == 1) { //right key
 		animationDirection = 1;
-		x += 5;
+		x += 20;
 		if (++frameCount > frameDelay)
 		{
 			frameCount = 0;
@@ -56,7 +56,7 @@ void Sprite::UpdateSprites(int width, int height, int dir)
 	}
 	else if (dir == 0) { //left key
 		animationDirection = 0;
-		x -= 5;
+		x -= 20;
 		if (++frameCount > frameDelay)
 		{
 			frameCount = 0;
